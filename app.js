7834a2a3ec99f0c9dc3aff375a3496c00d4c3bbb
iulon <i.magularia357@gmail.com>
2023-07-24 22:42:28 +0400

    გაკეთებულია 2 დავალება

---
+++ app.js
@@ -0,0 +1,27 @@
+// დავალება-1:
+
+function kvaDab (num) {
+    var kvadrati = num * num;
+    return kvadrati;
+    
+}
+
+var ricxvi = kvaDab(5);
+console.log("კვადრატში აყვანის შემდგომ მიღებული " + ricxvi);
+
+//----------------------------------------------
+
+// დავალება-2:
+
+function discount (a, b) {
+    var c = (b/100) * a;
+    var d = a - c;
+    return d;
+}
+
+var sul = discount(1000, 10);
+console.log("ფასდაკლების შემდეგ გადასახდელი თანხა არის " + sul); 
+
+//----------------------------------------------
+
+// დავალება-3:
