8a8257b1d0b807285e748da490f654c96155b753
iulon <i.magularia357@gmail.com>
2023-07-25 00:17:38 +0400

    შესრულებულია მე-5 დავალება

--- app.js
+++ app.js
@@ -46,3 +46,75 @@ console.log(sym);
 
 //----------------------------------------------
 
+// დავალება-5:
+
+function smallestRate (a, b, c, d, e){
+    var arr = [a, b, c, d, e];
+    var min = Math.min(...arr);
+    return min;
+}
+
+const currencies = [
+	{
+		code: "AMD",
+		quantity: 1000,
+		rateFormated: "6.6742",
+		diffFormated: "0.0202",
+		rate: 6.6742,
+		name: "სომხური დრამი",
+		diff: 0.0202,
+		date: "2023-07-21T17:45:10.772Z",
+		validFromDate: "2023-07-22T00:00:00.000Z",
+	},
+	{
+		code: "EUR",
+		quantity: 1,
+		rateFormated: "2.8673",
+		diffFormated: "0.0131",
+		rate: 2.8673,
+		name: "ევრო",
+		diff: -0.0131,
+		date: "2023-07-21T17:45:10.772Z",
+		validFromDate: "2023-07-22T00:00:00.000Z",
+	},
+	{
+		code: "GBP",
+		quantity: 1,
+		rateFormated: "3.3111",
+		diffFormated: "0.0046",
+		rate: 3.3111,
+		name: "დიდი ბრიტანეთის გირვანქა სტერლინგი",
+		diff: -0.0046,
+		date: "2023-07-21T17:45:10.772Z",
+		validFromDate: "2023-07-22T00:00:00.000Z",
+	},
+	{
+		code: "TRY",
+		quantity: 1,
+		rateFormated: "0.0957",
+		diffFormated: "0.0003",
+		rate: 0.0957,
+		name: "თურქული ლირა",
+		diff: 0.0003,
+		date: "2023-07-21T17:45:10.772Z",
+		validFromDate: "2023-07-22T00:00:00.000Z",
+	},
+
+	{
+		code: "USD",
+		quantity: 1,
+		rateFormated: "2.5771",
+		diffFormated: "0.0078",
+		rate: 2.5771,
+		name: "აშშ დოლარი",
+		diff: 0.0078,
+		date: "2023-07-21T17:45:10.772Z",
+		validFromDate: "2023-07-22T00:00:00.000Z",
+	},
+];
+
+var sR = smallestRate (currencies[0].rate, currencies[1].rate, currencies[2].rate, currencies[3].rate, currencies[4].rate);
+
+console.log(sR);
+
+//----------------------------------------------
